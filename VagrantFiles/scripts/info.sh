d8260900f62f968ba153091950c0def4626031d3
hassancoeus <muhammad.hassan@coeus-solutions.de>
2015-07-16 07:02:22 +0500

    Vagrantfile and Scripts

---
+++ VagrantFiles/scripts/info.sh
@@ -0,0 +1,3 @@
+echo "Welcome to the Vagrant Machine"
+echo "Make sure everything is pre installed"
+ls -l
---
+++ VagrantFiles/scripts/setup.sh
@@ -0,0 +1,24 @@
+echo "Thankyou for running me, lets just begin with installing few things Apache 2, MySQL and PHP"
+echo "Lets start with update"
+echo "Updating..."
+sudo apt-get update
+echo "Done with the Updates"
+echo "Installing Apache2..."
+sudo apt-get install apache2 -y
+#echo "Apache Successfully Installed"
+#echo "Lets Just move on to MySQL,Installing MySQL..."
+#sudo apt-get install mysql-server libapache2-mod-auth-mysql php5-mysql
+#echo "MySQL Successfully Installed"
+#echo "Activating MySQL..."
+#sudo mysql_install_db
+#sudo /usr/bin/mysql_secure_installation
+#echo "Done with the MySQL"
+#echo "Lets install PHP now"
+#echo "Installing PHP..."
+#sudo apt-get install php5 libapache2-mod-php5 php5-mcrypt
+##sudo nano /etc/apache2/mods-enabled/dir.conf
+#apt-cache search php5-
+#echo "PHP Successfully installed"
+#echo "Installing Vim..."
+#sudo apt-get install vim
+#echo "Congragulations Your Vargrant MAchine got fully configured with some latest tools"
