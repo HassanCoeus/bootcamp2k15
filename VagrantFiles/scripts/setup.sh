12e56dfd36bf532d29da26a55157f4824339c391
hassancoeus <muhammad.hassan@coeus-solutions.de>
2015-07-16 07:14:08 +0500

    Uncommenting Remaining Scripts

--- VagrantFiles/scripts/setup.sh
+++ VagrantFiles/scripts/setup.sh
@@ -5,20 +5,20 @@ sudo apt-get update
 echo "Done with the Updates"
 echo "Installing Apache2..."
 sudo apt-get install apache2 -y
-#echo "Apache Successfully Installed"
-#echo "Lets Just move on to MySQL,Installing MySQL..."
-#sudo apt-get install mysql-server libapache2-mod-auth-mysql php5-mysql
-#echo "MySQL Successfully Installed"
-#echo "Activating MySQL..."
-#sudo mysql_install_db
-#sudo /usr/bin/mysql_secure_installation
-#echo "Done with the MySQL"
-#echo "Lets install PHP now"
-#echo "Installing PHP..."
-#sudo apt-get install php5 libapache2-mod-php5 php5-mcrypt
-##sudo nano /etc/apache2/mods-enabled/dir.conf
-#apt-cache search php5-
-#echo "PHP Successfully installed"
-#echo "Installing Vim..."
-#sudo apt-get install vim
-#echo "Congragulations Your Vargrant MAchine got fully configured with some latest tools"
+echo "Apache Successfully Installed"
+echo "Lets Just move on to MySQL,Installing MySQL..."
+sudo apt-get install mysql-server libapache2-mod-auth-mysql php5-mysql
+echo "MySQL Successfully Installed"
+echo "Activating MySQL..."
+sudo mysql_install_db
+sudo /usr/bin/mysql_secure_installation -y
+echo "Done with the MySQL"
+echo "Lets install PHP now"
+echo "Installing PHP..."
+sudo apt-get install php5 libapache2-mod-php5 php5-mcrypt
+apt-cache search php5-
+sudo service apache2 restart
+echo "PHP Successfully installed"
+echo "Installing Vim..."
+sudo apt-get install vim
+echo "Congragulations Your Vargrant MAchine got fully configured with some latest tools"
